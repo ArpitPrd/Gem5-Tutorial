47ba54580e6da5e5de55f92ec0b3e13a87a35462
ArpitPrd <prd.arpit@gmail.com>
2025-08-15 14:06:21 +0530

    working shell script

---
+++ multiple_sims.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CPUS=(O3 TIMING) #ATOMIC)
+FREQS=(600MHz) #800MHz 1000MHz 1200MHz 1400MHz 1600MHz 1800MHz 2000MHz 2200MHz 2400MHz 2600MHz 2800MHz 3000MHz 3200MHz 3300MHz)
+MEMS=(DDR3) # DDR4 HBM)
+mode=a
+for cpu in "${CPUS[@]}"; do
+  for freq in "${FREQS[@]}"; do
+    for mem in "${MEMS[@]}"; do
+      /home/arpit/Desktop/iitd/sem_7/COL718/projects/gem5/build/X86/gem5.opt \
+        config_script.py \
+        --cpu_type="$cpu" \
+        --cpu_freq="$freq" \
+        --mem_type="$mem" \
+        --cache_type=MESITwoLevelCacheHierarchy \
+        --mem_size=2GiB \
+        --mode=$mode
+    done
+  done
+done
